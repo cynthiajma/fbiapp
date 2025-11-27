462862aae0d9ee15113deaceb65f09c77c83ac11
Cynthia Ma <cjm135@duke.edu>
2025-11-27 09:58:07 -0500

    fixed isProduction variable

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const { Pool } = require('pg');
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 const pool = new Pool({
   connectionString: isProduction
     ? process.env.DATABASE_URL   // Railway 
